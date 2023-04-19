daf8b62a92e783363bc271004834bec13243af87
emiliomorelieras <112988503+emiliomorelieras@users.noreply.github.com>
2023-04-18 19:11:44 -0600

    Add files via upload

---
+++ uNO NUEVO/main.js
@@ -0,0 +1,13 @@
+function setup(){canvas=createCanvas(280,280);
+    canvas.center();
+    background("red");canvas.mouseReleased(classifyCanvas);synth=window.speechSynthesis;
+}
+var timecounter();
+timecounter=0;
+var time_check();
+var drawn_sketch()
+var answer_holder()
+var answer_holder()
+var score();
+score=0;
+
